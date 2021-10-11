072cee82d1d830a14294ba952db19cd03fdb84a2
nils <nils.christian@web.de>
2021-10-11 02:04:18 +0200

    some funcs

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 "use strict";
+
+let fields=[];
+let currentShape='cross';
+let shapeShifter= function(shape){ 
+    return currentShape= shape=='cross'? 'circle':'cross';
+};
+
+function fillShape(id){
+   
+   fields[id]=shapeShifter(currentShape);
+   draw();
+}
+
+function draw(){
+    fields.forEach((shape,index)=>shape=='circle'?
+    document.getElementsByClassName(index)[0].classList.remove('d-none')
+    :
+    document.getElementsByClassName(index)[1].classList.remove('d-none')
+    )
+}
+
