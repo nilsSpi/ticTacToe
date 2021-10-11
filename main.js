9b9469382521d598cc80afd9d73d38fd68e4dea3
nils <nils.christian@web.de>
2021-10-11 03:09:29 +0200

    some more

--- main.js
+++ main.js
@@ -2,6 +2,17 @@
 
 let fields=[];
 let currentShape='cross';
+
+let checkRow1= array=>array[0]==array[1] && array[1]==array[2];
+let checkRow2= array=>array[3]==array[4] && array[4]==array[5];
+let checkRow3= array=>array[6]==array[7] && array[7]==array[8];
+let checkCol1= array=>array[0]==array[3] && array[3]==array[6];
+let checkCol2= array=>array[1]==array[4] && array[4]==array[7];
+let checkCol3= array=>array[2]==array[5] && array[5]==array[8];
+let checkLeftBotRightTop= array=>array[6]==array[4] && array[4]==array[2];
+let checkLefttTopRightBot= array=>array[0]==array[4] && array[4]==array[8];
+
+
 let shapeShifter= function(shape){ 
     return currentShape= shape=='cross'? 'circle':'cross';
 };
@@ -20,3 +31,17 @@ function draw(){
     )
 }
 
+let checkRowWinCondition= function (){
+   return checkRow1(fields) || checkRow2(fields) || checkRow3(fields);
+}
+let checkColWinCondition =function (){
+    return checkCol1(fields) || checkCol2(fields) || checkCol3(fields);
+ }
+ let checkDiagWinCondition =function (){
+    return checkLeftBotRightTop(fields) || checkLefttTopRightBot(fields);
+ }
+
+
+function checkWin(){
+    return checkRowWinCondition()||checkColWinCondition()||checkDiagWinCondition();
+}
